a010ab518e6ff12b238fc012d911a54034e888e4
Mikhail Viryasov <viryasov.mikhail@gmail.com>
2019-11-21 15:26:27 +0300

    Improved

--- main.cpp
+++ main.cpp
@@ -3,25 +3,24 @@
 #include <omp.h>
 
 int main() {
-	constexpr size_t n = 100000000;
-	std::vector<bool> numbers(n, true);
+	constexpr size_t n = 1000000000;
+	bool* prime_numbers = new bool[n];
+	size_t max_idx = sqrt(n);
+	for (size_t i = 0; i < n; ++i)
+		prime_numbers[i] = true;
 	for (auto num_threads : { 1,2,3,4 }) {
 		double start = omp_get_wtime();
-#pragma omp parallel for num_threads(num_threads)
-		for (int i = 2; i < numbers.size(); ++i) {
-			if (!numbers[i])
+#pragma omp parallel for num_threads(num_threads) schedule(dynamic)
+		for (int i = 2; i <= max_idx; ++i) {
+			if (!prime_numbers[i])
 				continue;
-			for (int j = 2*i; j < n; j += i)
-				numbers[j] = false;
+			for (int j = i * i; j < n; j += i)
+				prime_numbers[j] = false;
 		}
 		double end = omp_get_wtime();
 		std::cout << "Threads count: " << num_threads << " time: " << end - start << '\n';
-
-
-		/*for (int i = 0; i < numbers.size(); ++i)
-			if (numbers[i])
-				std::cout << i << ' ';*/
 	}
+	delete[] prime_numbers;
 	system("pause");
 	return 0;
 }
